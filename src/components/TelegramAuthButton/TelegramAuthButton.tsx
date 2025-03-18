8db3d78c2d825b90b6eb8486ffd9c5780fbe9064
Nicolae <ksirbu519@gmail.com>
2025-03-18 21:47:37 +0200

    fix auth

--- src/components/TelegramAuthButton/TelegramAuthButton.tsx
+++ src/components/TelegramAuthButton/TelegramAuthButton.tsx
@@ -2,12 +2,13 @@ import { Button } from "@mui/material";
 import React from "react";
 import TelegramIcon from "@mui/icons-material/Telegram";
 
-const TELEGRAM_BOT_ID = "7857873692";
+const TELEGRAM_BOT_USERNAME = "AuthBot_3232_bot";
+
 const REDIRECT_URI = "https://fragment-six.vercel.app/auth/callback";
 
 const TelegramAuthButton: React.FC = () => {
   const handleLogin = () => {
-    window.location.href = `https://oauth.telegram.org/auth?bot_id=${TELEGRAM_BOT_ID}&origin=${encodeURIComponent(
+    window.location.href = `https://oauth.telegram.org/auth?bot_username=${TELEGRAM_BOT_USERNAME}&origin=${encodeURIComponent(
       window.location.origin
     )}&embed=1&request_access=write&return_to=${encodeURIComponent(
       REDIRECT_URI
