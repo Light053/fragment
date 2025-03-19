09dcc90ef6659647af0d8842784f61d59c0df557
Nicolae <ksirbu519@gmail.com>
2025-03-19 09:42:00 +0200

    fix auth

--- src/components/TelegramAuthButton/TelegramAuthButton.tsx
+++ src/components/TelegramAuthButton/TelegramAuthButton.tsx
@@ -1,26 +1,17 @@
+import { LoginButton } from "@telegram-auth/react";
 import React, { useEffect } from "react";
 
 const TelegramAuthButton: React.FC = () => {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://telegram.org/js/telegram-widget.js?22";
-    script.async = true;
-    script.setAttribute("data-telegram-login", "AuthBot_3232_bot");
-    script.setAttribute("data-size", "large");
-    script.setAttribute("data-radius", "20");
-    script.setAttribute(
-      "data-auth-url",
-      "https://fragment-six.vercel.app/auth/callback"
-    );
-
-    const container = document.getElementById("telegram-button-container");
-    if (container) {
-      container.innerHTML = "";
-      container.appendChild(script);
-    }
-  }, []);
-
-  return <div id="telegram-button-container"></div>;
+  return (
+    <LoginButton
+      botUsername="AuthBot_3232_bot"
+      authCallbackUrl="https://fragment-six.vercel.app/auth/callback"
+      buttonSize="large"
+      cornerRadius={20}
+      showAvatar
+      lang="en"
+    />
+  );
 };
 
 export default TelegramAuthButton;
