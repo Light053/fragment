93e1d8bfbfe73b1ce83e41ab4578699c177e69ec
Nicolae <ksirbu519@gmail.com>
2025-03-19 09:53:57 +0200

    fix button

--- src/components/TelegramAuthButton/TelegramAuthButton.tsx
+++ src/components/TelegramAuthButton/TelegramAuthButton.tsx
@@ -5,14 +5,14 @@ const TelegramAuthButton: React.FC = () => {
     const script = document.createElement("script");
     script.src = "https://telegram.org/js/telegram-widget.js?22";
     script.async = true;
-    script.setAttribute("data-telegram-login", "samplebot");
+    script.setAttribute("data-telegram-login", "AuthBot_3232_bot");
     script.setAttribute("data-size", "large");
     script.setAttribute("data-radius", "14");
     script.setAttribute(
       "data-auth-url",
       "https://fragment-six.vercel.app/auth/callback"
     );
-
+    script.setAttribute("data-request-access", "write");
     document.getElementById("telegram-button-container")?.appendChild(script);
   }, []);
 
